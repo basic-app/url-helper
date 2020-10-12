a779f899b22296bce130135cd176fe70c7222e0e
basic-app <dev@basic-app.com>
2020-10-12 20:46:39 +0300

    n/a

---
+++ Helpers/url_helper.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Basic App Dev Team <dev@basic-app.com>
+ * @license MIT
+ * @link https://basic-app.com
+ */
+use Config\App;
+
+if (!function_exists('url'))
+{
+    function url(string $uri, array $params = [], string $scheme = null, App $alt = null)
+    {
+        return UrlHelper::createUrl($uri, $params, $scheme, $alt);
+    }
+}
---
+++ UrlHelper.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * @author Basic App Dev Team <dev@basic-app.com>
+ * @license MIT
+ * @link https://basic-app.com
+ */
+namespace BasicApp\UrlHelper;
+
+use Config\App;
+
+class UrlHelper
+{
+
+    public static function getCurrentLocale()
+    {
+        return service('request')->getLocale();
+    }
+
+    public static function createUrl($path, array $params = [], string $scheme = null, App $alt = null) : string 
+    {
+        helper(['url']);
+
+        if ($params)
+        {
+            $path .= '?' . http_build_query($params);
+        }
+
+        $config = config(App::class);
+
+        if ($config->defaultLocale)
+        {
+            $locale = static::getCurrentLocale();
+          
+            if ($locale && ($config->defaultLocale != $locale))
+            {
+                $path = $locale . '/' . $path;
+            }
+        }
+
+        return site_url($path, $scheme, $alt);
+    }
+
+    public static function applyGetParams(string $query, array $params = [])
+    {
+        $params_list = service('request')->getGet();
+
+        foreach($params as $key => $value)
+        {
+            $params_list[$key] = $value;
+        }
+
+        if ($params_list)
+        {
+            $query .= '?' . http_build_query($params_list);
+        }
+
+        return $query;
+    }
+
+    public static function getCurrentUri(bool $applyParams = false, array $params = [])
+    {
+        $return = uri_string();
+
+        $segments = explode('/', $return);
+
+        if (count($segments) > 0)
+        {
+            $locale = static::getCurrentLocale();
+
+            if ($segments[0] == $locale)
+            {
+                unset($segments[0]);
+
+                $return = implode('/', $segments);
+            }
+        }
+
+        if ($applyParams)
+        {
+            $return = static::applyGetParams($return, $params);
+        }
+
+        return $return;        
+    }    
+
+    public static function returnUrl($path, array $params = [], string $scheme = null, App $alt = null) : string
+    {
+        $query = static::getCurrentUri(true);
+
+        $params['returnUrl'] = $query;
+
+        return static::createUrl($path, $params, $scheme, $alt);
+    }
+
+    public static function currentUrl($params = [], string $scheme = null, App $alt = null)
+    {
+        $params = array_merge(service('request')->getGet(), $params);
+
+        return static::createUrl(static::getCurrentUri(), $params, $scheme, $alt);
+    }
+
+    public static function redirect(string $url, string $method = 'auto', int $code = null)
+    {
+        return service('response')->redirect($url, $method, $code);;
+    }
+
+}
